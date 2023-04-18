a4e7be1dcbf9b663cbbfaf8af80ba3e09bfe86b8
Austin Joiner <austin.j.joiner1@gmail.com>
2023-04-18 11:56:43 -0400

    added some data for albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,12 +2,13 @@ USE codeup_test_db;
 
 drop table if exists albums;
 CREATE TABLE albums (
-    id VARCHAR(50),
+    id int UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50),
     name VARCHAR(50),
     release_date VARCHAR(50),
     sales VARCHAR(50),
-    genre VARCHAR(50)
+    genre VARCHAR(50),
+    PRIMARY KEY (id)
 );
 
 
