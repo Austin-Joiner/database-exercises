67979be3e98fa14a7ecf45cc295d1d836bfc5308
Austin Joiner <austin.j.joiner1@gmail.com>
2023-04-18 13:15:49 -0400

    fixed errors

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,14 +1,14 @@
 USE codeup_test_db;
-
+/*mysql -u codeup_test_user -p < select_exercises.sql*/
 SELECT name FROM albums Where artist = 'Pink Floyd';
 
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
-SELECT genre FROM albums WHERE name = Nevermind;
+SELECT genre FROM albums WHERE name = 'Nevermind';
 
-SELECT * FROM albums WHERE release_date = release_date >= 1990 AND release_date <= 1999;
+SELECT * FROM albums WHERE release_date BETWEEN 1989 AND 2000;
 
-SELECT * FROM albums WHERE sales = sale < 20;
+SELECT * FROM albums WHERE sales < 20;
 
 SELECT * FROM albums WHERE genre = 'Rock';
 
