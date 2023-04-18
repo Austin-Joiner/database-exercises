8cfca28f8b3ad155133bfe561db1f3675aba435b
Austin Joiner <austin.j.joiner1@gmail.com>
2023-04-18 16:39:30 -0400

    finished exercises for basic statements.

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,5 +1,7 @@
 USE codeup_test_db;
 
-SELECT name AS 'Albums released after 1991' FROM albums WHERE release_date > 1991;
-SELECT name AS 'Albums with genre disco' FROM albums WHERE genre LIKE '%Disco%';
-SELECT name AS 'Albums By WHITNEY HOUSTON' FROM albums WHERE artist = 'Whitney Houston';
+DELETE FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE genre LIKE '%Disco%';
+DELETE FROM albums WHERE artist = 'Whitney Houston';
+
+SELECT * FROM albums;
