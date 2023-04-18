3edac92e81044a0ddddb962a70a03aa14e037b80
Austin Joiner <austin.j.joiner1@gmail.com>
2023-04-18 10:49:51 -0400

    finished tables

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+drop table if exists albums;
+CREATE TABLE albums (
+    id VARCHAR(50),
+    artist VARCHAR(50),
+    name VARCHAR(50),
+    release_date VARCHAR(50),
+    sales VARCHAR(50),
+    genre VARCHAR(50)
+);
+
+
