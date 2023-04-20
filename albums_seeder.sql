1542f2323b1aa1223aa060b9896afb6c0528b6f1
Austin Joiner <austin.j.joiner1@gmail.com>
2023-04-20 14:18:28 -0400

    commented out duplicate for index exercise

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -6,7 +6,7 @@ TRUNCATE TABLE albums;
 INSERT INTO albums (artist, name, release_date, sales, genre)
 VALUES ('Michael Jackson', 'Thriller', 1982, 51.2, 'Pop, post-disco, funk, rock'),
  ('AC/DC', 'Back in Black', 1980, 30.1, 'Hard rock'),
- ('AC/DC', 'Back in Black', 1980, 30.1, 'Hard rock'),
+--  ('AC/DC', 'Back in Black', 1980, 30.1, 'Hard rock'),
  ('Whitney Houston', 'The Bodyguard', 1992, 28.7, 'R&B, soul, pop, soundtrack'),
  ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.8, 'Progressive rock'),
  ('Eagles', 'Their Greatest Hits', 1976, 41.2, 'Country rock, soft rock, folk rock'),
