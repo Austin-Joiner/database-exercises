db80073a01716046e9e0c1f68e03be682f296c82
Austin Joiner <austin.j.joiner1@gmail.com>
2023-04-25 15:57:02 -0400

    finished exercise and bonuses

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -10,11 +10,11 @@ WHERE hire_date = (
     );
 
 -- question 2 solved
-SELECT  first_name, last_name, title
+-- JOIN employees as e
+--     ON t.emp_no = e.emp_no
+SELECT title
 FROM titles as t
-JOIN employees as e
-    ON t.emp_no = e.emp_no
-WHERE e.emp_no IN (
+WHERE emp_no IN (
     SELECT emp_no
     FROM employees
     WHERE first_name = 'Aamod'
@@ -22,6 +22,49 @@ WHERE e.emp_no IN (
 
 -- question 3
 SELECT first_name, last_name
+FROM employees
+WHERE gender = 'F' AND emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date = '9999-01-01' AND dept_no IN (
+        SELECT dept_no
+        FROM dept_emp
+    )
+);
+
+-- BONUS 1
+SELECT dept_name
+FROM departments as d
+    WHERE dept_no IN (
+        SELECT dept_no
+        FROM dept_manager
+        WHERE to_date = '9999-01-01' AND
+        emp_no IN (
+            SELECT emp_no
+            FROM employees
+            WHERE gender = 'F'
+                )
+        );
+-- BONUS 1 translated from a sub query to a join
+SELECT dept_name
+FROM departments as d
+         JOIN dept_manager as dm
+              ON dm.dept_no = d.dept_no
+         JOIN employees as e
+              ON dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01' AND gender = 'F';
 
 
+-- BONUS 2
+SELECT first_name, last_name
+FROM employees
+    WHERE emp_no IN
+          (SELECT emp_no
+           FROM salaries
+           WHERE salary =
+                 (
+                  SELECT MAX(salary)
+                  FROM salaries
+                 )
+           );
 
